d44a7c1ba3aafd816fed80a5a57d9facdc0a0010
Sergey <sergeylunkov@hotmail.com>
2018-10-24 23:51:00 +0300

    EventsAndDelegates: Delegate EventHandler<>

--- EventsAndDelegates/VideoEncoder.cs
+++ EventsAndDelegates/VideoEncoder.cs
@@ -14,9 +14,7 @@ namespace EventsAndDelegates
         // 2- Define an event based on that delegate
         // 3- Raise the event
 
-        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
-
-        public event VideoEncodedEventHandler VideoEncoded;
+        public event EventHandler<VideoEventArgs> VideoEncoded;
 
         public void Encode(Video video)
         {
